f69622eeb8dc76d54913d6ae13de76a1bd66385f
fancygasmine <43125655+fancygasmine@users.noreply.github.com>
2018-09-10 11:36:44 +0800

    Update download.sh
    1) change Jenkins base image version to 2.141 for compatible issue of installing Jenkins plugins
    2) change the maven version and download web URL from 3.5.0 to 3.0.5

--- download.sh
+++ download.sh
@@ -3,12 +3,12 @@
 #clean anything with same name to get rid of clashes
 docker-compose down
 
-docker pull jenkins:2.60.1
+docker pull jenkins/jenkins:2.141
 docker pull sonarqube:6.3.1
 docker pull docker.bintray.io/jfrog/artifactory-oss:5.4.4
 
 if [ ! -d downloads ]; then
     mkdir downloads
 #    curl -o downloads/jdk-8u131-linux-x64.tar.gz http://ftp.osuosl.org/pub/funtoo/distfiles/oracle-java/jdk-8u131-linux-x64.tar.gz
-    curl -o downloads/apache-maven-3.5.0-bin.tar.gz http://apache.mirror.anlx.net/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz
+    curl -o downloads/apache-maven-3.0.5-bin.tar.gz http://apache.mirror.anlx.net/maven/maven-3/3.0.5/binaries/apache-maven-3.0.5-bin.tar.gz
 fi
